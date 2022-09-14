89d0eb8b806c339b3c11d69966ca27735d749adf
Lucas Anselmo <30813248+lucas62@users.noreply.github.com>
2022-09-14 20:39:11 -0300

    Add Header component

---
+++ mobile/src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import { Text, View, ViewProps } from 'react-native';
+
+import { styles } from './styles';
+
+interface Props extends ViewProps {
+    title: string;
+    subtitle: string;
+}
+
+/**
+ * This function takes in a title and subtitle and returns a View with a title and
+ * subtitle.
+ * @param {Props}  - Props
+ * @returns A View component with a title and subtitle.
+ */
+export function Header({ title, subtitle, ...rest }: Props ) {
+  return (
+    <View style={styles.container} {...rest}>
+        <Text style={styles.title}>
+            {title}
+        </Text>
+        <Text style={styles.subtitle}>
+            {subtitle}
+        </Text>
+    </View>
+  );
+}
